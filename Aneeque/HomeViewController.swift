285411f12c4f0f7fbec9d70fe3183560f3280fab
Bakare32 <bakarewariz012@gmail.com>
2021-10-24 23:46:42 +0100

    set up the screen delegate and rename the home view controller

---
+++ Aneeque/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aneeque
+//
+//  Created by  Decagon on 24/10/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
